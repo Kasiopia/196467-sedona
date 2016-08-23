1fef7e807061fee285f8a8ce7952eec486c9b072
Kasiopia <scout1982@mail.ru>
2016-08-23 18:53:22 +0200

    недоделал под пиксел перф 2 страницу

--- js/js.js
+++ js/js.js
@@ -15,22 +15,5 @@
   }
 });
 
-    jQuery(document).ready(function($) {
-        $('.minus').click(function () {
-            var $input = $(this).parent().find('input');
-            var val = +$input[0].defaultValue;
-            var count = parseInt($input.val()) - val;
-            count = count < val ? val : count;
-            $input.val(count);
-            $input.change();
-            return false;
-        });
-        $('.plus').click(function () {
-            var $input = $(this).parent().find('input');
-            var val = +$input[0].defaultValue;
-            $input.val(parseInt($input.val()) + val);
-            $input.change();
-            return false;
-        });
-    });
+
 
