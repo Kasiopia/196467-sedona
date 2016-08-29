53a4c6cf63eba45af2990a9d8c2986e77cea4fdb
Kasiopia <scout1982@mail.ru>
2016-08-29 22:40:17 +0300

    d

--- js/js.js
+++ js/js.js
@@ -4,16 +4,12 @@
     linkforma.addEventListener ("click", function (event) {
       event.preventDefault();
       form.classList.add ("show-map");
-  });
-
+    });
 
     window.addEventListener ("keydown", function(event) {
       if (event.keyCode === 27) {
         if (form.classList.contains ("show-map")) {
           form.classList.remove ("show-map");
+        }
       }
-  }
-});
-
-
-
+    });
